164ab6c25782ac047d777dc1ed18e9b074f14b4b
Trevor McNamara <trevormcnamara@email.arizona.edu>
2020-11-04 13:20:45 -0700

    scss

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./landing.css";
+import "./landing.scss";
 
 function Landing() {
   const [registerUsername, setRegisterUsername] = useState("");
--- client/src/pages/notes.js
+++ client/src/pages/notes.js
@@ -1,5 +1,6 @@
 import React,{useState, useEffect} from "react";
 import axios from "axios";
+import "./notes.scss";
 
 function Notes() {
     const [notes, setNotes]= useState([]);
